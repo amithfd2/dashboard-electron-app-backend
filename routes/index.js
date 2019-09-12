cf12b928316f584752de53f7ee91d7b232c96874
Abhilash <abhilash@spericorn.com>
2019-09-12 19:30:27 +0530

    api response modification

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,5 @@ var express = require('express');
 var router = express.Router();
 
 router.use('/cognito', require('../controllers/api/v1/cognito'));
-// router.use('/auth', require('../controllers/auth'));
 
 module.exports = router;
